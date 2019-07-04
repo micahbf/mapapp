20e4dc5c9135d1fa848ea0a0a6e9f59c18fa391b
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2019-07-04 13:57:16 -0700

    Initial stab at SpotAPI

---
+++ src/spot_api.ts
@@ -0,0 +1,54 @@
+import { RestClient } from 'typed-rest-client/RestClient'
+
+export interface SpotMessage {
+  id: number
+  messengerId: string
+  unixTime: number
+  messageType: string
+  latitude: number
+  longitude: number
+  modelId: string
+  showCustomMsg: string
+  dateTime: string
+  messageDetail: string
+  batteryState: string
+  hidden: number
+  altitude: number
+}
+
+interface SpotFeed {
+  id: string
+  name: string
+  description: string
+  status: string
+  usage: number
+  daysRange: number
+  detailedMessageShown: boolean
+  type: string
+}
+
+interface SpotFeedResponse {
+  response: {
+    feedMessageResponse: {
+      count: number
+      feed: SpotFeed
+      totalCount: number
+      activityCount: number
+      messages: {
+        message: SpotMessage[]
+      }
+    }
+  }
+}
+
+export class SpotAPI {
+  public static async getLatestMessages(feedId: string) {
+    const client = new RestClient(SpotAPI.userAgent, SpotAPI.baseUrl)
+    const response = await client.get<SpotFeedResponse>(`${feedId}/message.json`)
+
+    return response.result.response.feedMessageResponse.messages.message
+  }
+
+  private static userAgent = 'mapapp (micah.motorcycles)'
+  private static baseUrl = 'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/'
+}

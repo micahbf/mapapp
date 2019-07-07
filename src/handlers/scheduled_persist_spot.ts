5076c1279ee583365f76e38ca43e2567f5831028
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2019-07-07 10:21:37 -0700

    Fix handler names

--- src/handlers/scheduled_persist_spot.ts
+++ src/handlers/scheduled_persist_spot.ts
@@ -2,7 +2,7 @@ import { ScheduledHandler } from 'aws-lambda'
 import 'source-map-support/register'
 import { persistSpot } from '../spot_persistence'
 
-export const handler: ScheduledHandler = async () => {
+export const handle: ScheduledHandler = async () => {
   const result = await persistSpot()
   console.log(result)
 }

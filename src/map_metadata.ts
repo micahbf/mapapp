f8921590014a655c7cb1b54b76fc7efaf424c6f5
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2019-10-24 13:54:33 +0000

    Just send coordinates, not full points

--- src/map_metadata.ts
+++ src/map_metadata.ts
@@ -1,11 +1,12 @@
+import { Position } from 'geojson'
 import { lastMessagePerDay, lastUpdatePointFromMessages } from './map_data'
 import { FormattedMessage } from './spot_format'
 
-export function mapBounds(messages: FormattedMessage[], days: number): [GeoJSON.Point, GeoJSON.Point] {
+export function mapBounds(messages: FormattedMessage[], days: number): [Position, Position] {
   const lastUpdate = messages[messages.length - 1]
   const lastMessagesPerDay = lastMessagePerDay(messages)
   const targetBound = lastMessagesPerDay.reverse()[lastMessagesPerDay.length - days]
-  return [targetBound.point, lastUpdate.point]
+  return [targetBound.point.coordinates, lastUpdate.point.coordinates]
 }
 
 export function lastUpdateCaption(messages: FormattedMessage[]): string {
